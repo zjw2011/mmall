416e0117b51c3c53c3c6f9fdb51846ed76562df5
sanye <909861882@qq.com>
2018-06-14 14:18:44 +0800

    增加配置

---
+++ mmall-common/src/main/java/com/arunqi/mmall/common/HelloApplication.java
@@ -0,0 +1,14 @@
+package com.arunqi.mmall.common;
+
+/**
+ * 测试.
+ *
+ * @author jiawei zhang
+ * @datetime 2018/6/14 下午2:04
+*/
+public class HelloApplication {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("aaa");
+    }
+}
